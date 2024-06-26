587ffa3ba46e207bb450c76ef4af2dab60991e2a
Marlon Abeln <maab@netcompany.com>
2024-06-26 18:47:54 +0200

    Update to runner to default to newest Solution

--- src/main/java/aoc/AdventOfCode2022Solutions.java
+++ src/main/java/aoc/AdventOfCode2022Solutions.java
@@ -6,23 +6,52 @@ import aoc.framework.solution.AOCSolution;
 import aoc.framework.solution.Solution;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class AdventOfCode2022Solutions {
 
     public static void main(String[] args) {
         try {
-            AdventOfCode2022Solutions.solveAll();
+
+            if (Arrays.asList(args).contains("BATCH_MODE")) {
+                AdventOfCode2022Solutions.solveAll();
+            } else {
+                AdventOfCode2022Solutions.solveNewest();
+            }
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         System.exit(0);
     }
 
+    private static void solveNewest() throws AOCException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Optional<Integer> highestYear = getAllYears().stream().max(Comparator.naturalOrder())
+                .stream()
+                .findAny();
+
+        if (highestYear.isEmpty()) {
+            System.out.println("No years found in the solution!");
+            return;
+        }
+        Optional<Class<? extends AOCSolution>> currentSolution = getAllSolutionsForYear(highestYear.get()).stream()
+                .max(Comparator.comparingInt((Class<? extends AOCSolution> o) -> o.getAnnotation(Solution.class).day()));
+
+        if (currentSolution.isEmpty()) {
+            System.out.println("No days found in the newest year!");
+            return;
+        }
+
+        System.out.printf("Running Solution: Year: %s, Day %s %n",
+                currentSolution.get().getAnnotation(Solution.class).year(),
+                currentSolution.get().getAnnotation(Solution.class).day()
+        );
+        runSolution(currentSolution.get());
+    }
+
     /**
-     * Runs all advent of code solutions
+     * Runs all advent of code solutions*
      *
      * @throws AOCException
      * @throws NoSuchMethodException
@@ -36,21 +65,24 @@ public class AdventOfCode2022Solutions {
             List<Class<? extends AOCSolution>> days = getAllSolutionsForYear(year);
             for (Class<? extends AOCSolution> day : days) {
                 System.out.printf("\t >Day %s %n", day.getAnnotation(Solution.class).day());
-
-                AOCSolution currentSolution = day.getConstructor(boolean.class).newInstance(false);
-                long startTime = System.currentTimeMillis();
-                String solutionPart1 = currentSolution.solvePart1();
-                long endTime = System.currentTimeMillis();
-
-                System.out.printf("\t\t> Part 1: %s Runtime: %sms \n", solutionPart1, endTime - startTime);
-
-                startTime = System.currentTimeMillis();
-                String solutionPart2 = currentSolution.solvePart2();
-                endTime = System.currentTimeMillis();
-
-                System.out.printf("\t\t> Part 2: %s Runtime: %sms \n", solutionPart2, endTime - startTime);
+               runSolution(day);
             }
         }
+    }
+
+    private static void runSolution(Class<? extends AOCSolution> currentSolution) throws AOCException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        AOCSolution currentSolutionInstance = currentSolution.getConstructor(boolean.class).newInstance(false);
+        long startTime = System.currentTimeMillis();
+        String solutionPart1 = currentSolutionInstance.solvePart1();
+        long endTime = System.currentTimeMillis();
+
+        System.out.printf("\t\t> Part 1: %s Runtime: %sms \n", solutionPart1, endTime - startTime);
+
+        startTime = System.currentTimeMillis();
+        String solutionPart2 = currentSolutionInstance.solvePart2();
+        endTime = System.currentTimeMillis();
+
+        System.out.printf("\t\t> Part 2: %s Runtime: %sms \n", solutionPart2, endTime - startTime);
     }
 
     private static List<Integer> getAllYears() {
